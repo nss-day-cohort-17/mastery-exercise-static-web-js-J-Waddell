1b34ba3461851c8e7402d70c2d2c770b4185e6bf
J-Waddell <waddelljesse1@gmail.com>
2016-12-01 14:39:09 -0600

    color/symbol in console

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-var color = red
-var symbol = *
+var color = ["red"];
+console.log(color);
+
+var symbol = ["*"];
+console.log(symbol);
 
 var listSymbol = "";
 
+for (var i = 0; i < symbol.length; i ++) {
+        symbol[i]
+}
