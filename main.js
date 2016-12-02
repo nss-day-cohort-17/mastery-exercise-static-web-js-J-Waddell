80c7e7aebba7b380afe2061a07f7d7ec295bdf0a
J-Waddell <waddelljesse1@gmail.com>
2016-12-02 10:39:39 -0600

    added event

--- main.js
+++ main.js
@@ -10,14 +10,29 @@ for (var i = 0; i < symbol.length; i ++) {
         symbol[i]
 }
 
-document.addEventListener ("keypress", function (kbEvt) {
-        if (kbEvt.code === ' ', 'enter') {
-                console.log('Your tree is growin!')
-        }
-})
+var event = document.createEvent('Event');
+
+// Define that the event name is 'build'.
+event.initEvent('build', true, true);
+
+// Listen for the event.
+elem.addEventListener('build', function (e) {
+  // e.target matches elem
+}, false);
+
+// target can be any Element or other EventTarget.
+elem.dispatchEvent(event);
+
+
+
+// document.addEventListener ("keypress", function (kbEvt) {
+//         if (kbEvt.code === ' ', 'enter') {
+//                 console.log('Your tree is growin!')
+//         }
+// })
 
 // document.onkeypress = function (kbEvt) {
 //         if (kbEvt.key.toLowerCase() ==='return', ' ') {
-//                 alert ('Your tree is growin!')
+//                 alert ('You gotta plant your height and character seeds first.')
 //         }
 // }
