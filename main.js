dd2af6afb5a54bf7d429bc4ac73b1d3969c80e72
J-Waddell <waddelljesse1@gmail.com>
2016-12-02 10:12:02 -0600

    bootstrap added/deleted sum divs/listener

--- main.js
+++ main.js
@@ -10,8 +10,14 @@ for (var i = 0; i < symbol.length; i ++) {
         symbol[i]
 }
 
-document.onkeypress = function (kbEvt) {
-        if (kbEvt.key.toLowerCase() ==='return', ' ') {
-                alert ('Your tree is growin!')
+document.addEventListener ("keypress", function (kbEvt) {
+        if (kbEvt.code === ' ', 'enter') {
+                console.log('Your tree is growin!')
         }
-}
+})
+
+// document.onkeypress = function (kbEvt) {
+//         if (kbEvt.key.toLowerCase() ==='return', ' ') {
+//                 alert ('Your tree is growin!')
+//         }
+// }
